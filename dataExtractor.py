6e96ba7bb6f0dc0bd61f189dac2aa26f40dad231
Kabourlix <hdamaia@outlook.fr>
2021-03-31 09:17:21 +0200

    Create dataExtractor.py
    Init the dataset class for our project

---
+++ dataExtractor.py
@@ -0,0 +1,10 @@
+import torch
+
+class ExtractorDataSet():
+    def __init__(self,tab):
+
+    def __len__():
+        return len(self.tab)
+    
+    def __getitem__(idx):
+        return torch.from_numpy(self.tab[idx])
