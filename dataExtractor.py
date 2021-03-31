9bd422109fe3d49c6638cca325878d069b1f62c0
Kabourlix <hdamaia@outlook.fr>
2021-03-31 09:41:38 +0200

    Update dataExtractor.py
    Update of the class with what I've done on the other pc.

--- dataExtractor.py
+++ dataExtractor.py
@@ -1,10 +1,24 @@
 import torch
 
-class ExtractorDataSet():
-    def __init__(self,tab):
+class TrajDataSet():
+	"""
+	This class create our dataSet for our network. We will then create a DataLoader via this dataset so as to 
+	train our network.
+	"""
+	def __init__(self, traj,transform=None):
+		"""
+		It is the constructor of our class. 
+		traj is a numpy array containing all the position of n trajectories. It is a [n x len(trajectoire)] array. 
+		"""
+		self.array = traj
+		#Voir les autres attributs nécessaires. Cf tuto. 
 
-    def __len__():
-        return len(self.tab)
-    
-    def __getitem__(idx):
-        return torch.from_numpy(self.tab[idx])
+
+	def __len__():
+		return len(array)
+
+	def __getitem__(idx):
+		"""
+		This function return the idx-th trajectory of the array. As a tensor. 
+		"""
+		return torch.from_numpy(array[idx])
