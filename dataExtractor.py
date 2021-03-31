9bdacd2d7040e65489cae40fa9d20bf37b34491a
Kabourlix <hdamaia@outlook.fr>
2021-03-31 10:44:54 +0200

    update dataSet + add data
    Version 1 du dataSet
    Ajout des données expertes.

--- dataExtractor.py
+++ dataExtractor.py
@@ -1,24 +1,26 @@
 import torch
+import panda as pd
 
 class TrajDataSet():
 	"""
 	This class create our dataSet for our network. We will then create a DataLoader via this dataset so as to 
 	train our network.
 	"""
-	def __init__(self, traj,transform=None):
+	def __init__(self,trajectory,transform=None):
 		"""
 		It is the constructor of our class. 
-		traj is a numpy array containing all the position of n trajectories. It is a [n x len(trajectoire)] array. 
+		traj is a set of the following form [(s0,a0),(s1,a1),...,(sn,an)] with si a position and ai a speed vector.
 		"""
-		self.array = traj
-		#Voir les autres attributs nécessaires. Cf tuto. 
+		self.array = trajectory
+
+		#Je ne suis pas très sûr des données à mettre dedans, on va tester avec ça.  
 
 
 	def __len__():
-		return len(array)
+		return len(self.array)
 
 	def __getitem__(idx):
 		"""
-		This function return the idx-th trajectory of the array. As a tensor. 
+		This function return the idx-th pairs state/action of the array as a tensor. 
 		"""
-		return torch.from_numpy(array[idx])
+		return torch.from_numpy(self.array[idx])
