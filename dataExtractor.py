c625317376611197d22e72f018569992749ddd0c
Kabourlix <hdamaia@outlook.fr>
2021-04-14 12:19:14 +0200

    Update dataExtractor.py
    Add a class to extract our data and put it in TrajDataSet

--- dataExtractor.py
+++ dataExtractor.py
@@ -7,14 +7,14 @@ class TrajDataSet(torch.utils.data.Dataset):
 	This class create our dataSet for our network. We will then create a DataLoader via this dataset so as to 
 	train our network.
 	"""
-	def __init__(self,file_name,transform=None):
+	def __init__(self,data,transform=None):
 		"""
 		It is the constructor of our class. 
-		
+		Inputs : 
+			- data : DataFrame from pandas containing lon,lat,step_speed and step_direction. 
 		traj is a set of the following form [(s0,a0),(s1,a1),...,(sn,an)] with si a position and ai a speed vector.
 
 		"""
-		data = pd.read_csv(file_name)
 		self.traj = np.array([data.lon,data.lat,data.step_speed,data.step_direction]).transpose()
 
 	def __len__(self):
@@ -27,18 +27,53 @@ class TrajDataSet(torch.utils.data.Dataset):
 		return (torch.from_numpy(self.traj[idx][:2]),torch.from_numpy(self.traj[idx][2:])) #We output a tuple of tensor (state,action)
 		#I've got an unexpected error where torch.from_numpy is not recognized by my python interpreter (in VisualCode), don't know why. 
 
-######## Séparation du jeu de données #########
-data = TrajDataSet('trips_SV_2008_2015') #Import dataset
+class DataAdjust():
+	"""
+	This class enables to deal with our data. 
+	"""
+	def __init__(self,file_name,label_name = 'trip',drop_label = ['dive','prediction'],method=pd.read_csv):
+		self.data = method(file_name) #Init our data frame.
+		self.data.drop(drop_label,axis=1) #We delete the useless columns for our work. 
+		
+		self.label = self.data[label_name] #Init our label serie
+		self.label.drop_duplicates(keep='first',inplace=True)
+		
+		self.nb_label = self.label.shape[0] #Ammount of labels
+		
+		self.label_name = label_name
+	
+	def select_random_traj(self):
+		"""
+		Parameters
+		----------
+		label_name : str, optional
+			This is the label of the column we want to take our data on. 
+			At default, this is set on 'trip' which is associated to our dataset. 
 
-#Create trip labels
-trip_label = data['trip']
-trip_label.drop_duplicates(keep='first',inplace=True)
-nb_label = trip_label.shape()[0]
+		Returns
+		-------
+		pandas.dataFrame
+			This returns a dataFrame form of a unique trajectory
 
-def select_random_traj(dataSet, )
- 
+		"""
+		return self.data[self.data[self.label_name] == self.label.sample().iloc[0]]
+	
+	def subset_data(self,first_ammount):
+		extract_labels = self.label.sample(first_ammount)
+		mask = self.data[self.label_name].isin(extract_labels)
+		data_1 = self.data[mask]
+		data_2 = self.data[~mask]
+		return (data_1,data_2)
 
+# ######## Séparation du jeu de données #########
+# data = TrajDataSet('trips_SV_2008_2015') #Import dataset
 
+# #Create trip labels
+# trip_label = data['trip']
+# trip_label.drop_duplicates(keep='first',inplace=True)
+# nb_label = trip_label.shape()[0]
+
+# def select_random_traj(dataSet, )
 
 #Création du dataset
 #data = pd.read_csv("trips_SV_2008_2015.csv")
