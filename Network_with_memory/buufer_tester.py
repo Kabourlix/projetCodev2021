cf442792a6e3b821c27f058e4d891672aa7adbb3
Kabourlix <hdamaia@outlook.fr>
2021-06-01 19:00:09 +0200

    Output shape of dataset working fine

--- Network_with_memory/buufer_tester.py
+++ Network_with_memory/buufer_tester.py
@@ -4,9 +4,10 @@ import torch
 a = de.DataAdjust('data/test_data.csv',drop=False)
 
 test = de.TrajDataSet(a.get_data_Frame(),mem_nb=3)
-test_loader = torch.utils.data.DataLoader(test,batch_size=1,shuffle=True)
+test_loader = torch.utils.data.DataLoader(test,batch_size=1,shuffle=False)
 state = next(iter(test_loader))
-exemple = test[10]
+exemple = test[0]
 print(f'\n---------------\n Voici notre état \n {state} \n ---------------')
 print(f'Voici ce que nous renvoie le 10 élément de test \n {exemple} \n ---------------')
-print(f'Le contenu de test avant le passage à torch : \n {test.get_traj()[10:13]} \n ---------------')
+print(f'Le contenu de test avant le passage à torch : \n {test.get_traj()[0:3]} \n ---------------')
+print(f'La shape de notre Tenseur renvoyé par le test_loader : state {state[0].shape} et action {state[1].shape}')
